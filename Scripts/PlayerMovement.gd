c1e4bf378974f703b9c08567346000327c0db606
Xavier <96898473+xavman096@users.noreply.github.com>
2024-08-07 13:24:14 +1000

    Pushed code that doesnt work :)

--- Scripts/PlayerMovement.gd
+++ Scripts/PlayerMovement.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+var AttackCooldown = 0.6;
+var CharacterDirection = 1
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -17,6 +19,9 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+	if Input.is_action_just_pressed("Attack") and AttackCooldown <= 0:
+		pass
+		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("Backward", "Forward")
@@ -24,5 +29,14 @@ func _physics_process(delta):
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
+	# this doesnt currently work but im too lazy to fix it 
+	if direction == 1 and CharacterDirection == -1:
+		get_parent().flip_h = true
+		CharacterDirection = -1
+	
+	if direction == -1 and CharacterDirection == 1:
+		get_parent().flip_h = false
+		CharacterDirection = 1
+		
 	move_and_slide()
