4d22700503e4be3946386405bb13f76638c36d1d
Xavier <96898473+xavman096@users.noreply.github.com>
2025-02-07 20:09:24 +1100

    the return

--- Scripts/PlayerMovement.gd
+++ Scripts/PlayerMovement.gd
@@ -3,8 +3,9 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-var AttackCooldown = 0.6;
+var AttackCooldown = 2
 var CharacterDirection = 1
+var CanAttack = false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -18,9 +19,6 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	if Input.is_action_just_pressed("Attack") and AttackCooldown <= 0:
-		pass
 		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -28,15 +26,35 @@ func _physics_process(delta):
 	if direction:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, SPEED)	
 	
-	# this doesnt currently work but im too lazy to fix it 
 	if direction == 1 and CharacterDirection == -1:
-		get_parent().flip_h = true
-		CharacterDirection = -1
+		self.get_node("Player").flip_h = false
+		self.get_node("CollisionShape2D").position.x = 0
+		CharacterDirection = 1
 	
 	if direction == -1 and CharacterDirection == 1:
-		get_parent().flip_h = false
-		CharacterDirection = 1
+		self.get_node("Player").flip_h = true
+		self.get_node("CollisionShape2D").position.x = -42.25
+		CharacterDirection = -1
 		
+	if Input.is_action_just_pressed("Attack") and AttackCooldown <= 0:
+		print("Attacked")
+		# For animation to play when attacking but no hitting anything
+		
+		
+	AttackCooldown = AttackCooldown - 0.2
 	move_and_slide()
+
+	if Input.is_action_just_pressed("Attack") and AttackCooldown <= 0 and CanAttack == true:
+		AttackCooldown = 2
+		get_parent().get_node("Enemy").queue_free()
+		print("Destroyed Item")
+
+func _on_area_2d_area_entered(_area):
+	CanAttack = true
+	print("Player entered enemy")
+
+func _on_area_2d_area_exited(_area):
+	CanAttack = false
+	print("Player exited enemy")
