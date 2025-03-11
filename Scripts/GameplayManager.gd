91fb4502223633cc62083af7c908abd69ea515da
Xavier <96898473+xavman096@users.noreply.github.com>
2025-03-11 12:00:47 +1100

    .

--- Scripts/GameplayManager.gd
+++ Scripts/GameplayManager.gd
@@ -18,6 +18,7 @@ var Duplicate
 var FruitNumberGenerator = RandomNumberGenerator.new()
 var FruitNumber
 var FruitDuplicate
+var FruitExists = false
 
 var StartingPrefab
 
@@ -46,16 +47,26 @@ func _physics_process(_delta):
 	BoundryCooldown = BoundryCooldown - 1
 
 func RemovePrefab():
+
 	if $Player.CharacterDirection == 1:
+		if FruitExists == true:
+			if FruitDuplicate.position == Vector2(CurrentPrefabs[0].position.x + 736, -64):
+				FruitDuplicate.queue_free()
+				FruitExists = false
 		CurrentPrefabs[0].queue_free()
 		CurrentPrefabs.remove_at(0)
 		BoundryCooldown = 60
+
 		
 	if $Player.CharacterDirection == -1:
+		if FruitExists == true:
+			if FruitDuplicate.position == Vector2(CurrentPrefabs[2].position.x + 736, -64):
+				FruitDuplicate.queue_free()
+				FruitExists = false
 		CurrentPrefabs[2].queue_free()
 		CurrentPrefabs.remove_at(2)
 		BoundryCooldown = 60
-	
+
 	BoundryCooldown = 60
 	SpawnPrefab()
 
@@ -86,27 +97,24 @@ func SpawnPrefab():
 			add_child(Duplicate)
 			Duplicate.position = Vector2(($Player.BoundryPosition.x + 1512), 0)
 			CurrentPrefabs.insert(2, Duplicate)
-			if Number != 0:
-				FruitNumber = FruitNumberGenerator.randi_range(0,2)
-			if FruitNumber == 0:
-				FruitDuplicate = $FruitBowl.duplicate()
-				FruitDuplicate.name = 'FruitDuplicate'
-				FruitDuplicate.position = Vector2((Duplicate.position.x + 736), -64)
-				add_child(FruitDuplicate)
-				print("Spawned Fruit")
+
 			
 		if $Player.CharacterDirection == -1:
 			add_child(Duplicate)
 			Duplicate.position = Vector2(($Player.BoundryPosition.x - 3047), 0)
 			CurrentPrefabs.insert(0, Duplicate)
+			
+		if FruitExists == false:
 			if Number != 0:
-				FruitNumber = FruitNumberGenerator.randi_range(0,2)
+				FruitNumber = FruitNumberGenerator.randi_range(0,1)
 			if FruitNumber == 0:
 				FruitDuplicate = $FruitBowl.duplicate()
-				FruitDuplicate.name = "FruitDuplicate"
-				FruitDuplicate.position = Vector2((Duplicate.position.x - 736), -64)
+				FruitDuplicate.name = 'FruitDuplicate'
+				FruitDuplicate.position = Vector2((Duplicate.position.x + 736), -64)
 				add_child(FruitDuplicate)
+				FruitExists = true
 				print("Spawned Fruit")
+				print(FruitDuplicate.position)
 				
 		StartingPrefab = false
 		$Player.BoundryCollision = false
