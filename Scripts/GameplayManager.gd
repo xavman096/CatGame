68bcf2f7b06a0780cadec02852d7587dda95011e
Xavier <96898473+xavman096@users.noreply.github.com>
2025-04-16 22:50:45 +1000

    .

--- Scripts/GameplayManager.gd
+++ Scripts/GameplayManager.gd
@@ -26,6 +26,8 @@ var FruitArray = []
 var Apple
 var Pear
 var Banana
+var Bowlpopped = false
+var FruitDespawnTimer = 800
 
 var StartingPrefab
 
@@ -52,13 +54,22 @@ func _ready():
 func _physics_process(_delta):
 	if $Player.BoundryCollision == true and BoundryCooldown <= 0:
 		RemovePrefab()
+	BoundryCooldown = BoundryCooldown - 1
 	
 	if $Player.PlayerAttacked == true:
 		FruitScatter()
 		$Player.PlayerAttacked = false
 		
-	BoundryCooldown = BoundryCooldown - 1
-
+	if Bowlpopped == true:
+		if FruitDespawnTimer <= 0:
+			print("Removing Fruit")
+			print(FruitArray)
+			print("Array size is 0")
+			for i in FruitArray:
+				i.queue_free()
+			FruitDespawnTimer = 800
+		FruitDespawnTimer = FruitDespawnTimer - 1
+		
 func RemovePrefab():
 	if $Player.CharacterDirection == 1:
 		if FruitExists == true:
@@ -68,7 +79,6 @@ func RemovePrefab():
 		CurrentPrefabs[0].queue_free()
 		CurrentPrefabs.remove_at(0)
 		BoundryCooldown = 60
-
 		
 	if $Player.CharacterDirection == -1:
 		if FruitExists == true:
@@ -125,6 +135,8 @@ func SpawnPrefab():
 				FruitDuplicate.position = Vector2((Duplicate.position.x + 736), -64)
 				add_child(FruitDuplicate)
 				FruitExists = true
+				Bowlpopped = false
+				FruitArray = [Apple, Pear, Banana]
 				print("Fruit Spawned")
 		$Player.BoundryCollision = false
 		
@@ -133,22 +145,26 @@ func FruitScatter():
 		for i in range(0,3):
 			FruitDropSpot = RandomNumberGenerator.new().randf_range(FruitDuplicate.position.x - 300, FruitDuplicate.position.x + 300)
 			FruitArray[i].position = Vector2(FruitDropSpot, FruitDuplicate.position.y + 100)
+			Bowlpopped = true
 		get_node("FruitDuplicate").queue_free()
 		PlayerScore = PlayerScore + 50
 		FruitExists = false
 		$Player.AttackedObject = ""
 	
 	if $Player.AttackedObject == "Apple":
+		FruitArray.erase("Apple")
 		Apple.queue_free()
 		PlayerScore = PlayerScore + 100
 		$Player.AttackedObject = ""
 	
 	if $Player.AttackedObject == "Pear":
+		FruitArray.erase("Pear")
 		Pear.queue_free()
 		PlayerScore = PlayerScore + 100
 		$Player.AttackedObject = ""
 		
 	if $Player.AttackedObject == "Banana":
+		FruitArray.erase("Banana")
 		Banana.queue_free()
 		PlayerScore = PlayerScore + 100
 		$Player.AttackedObject = ""
